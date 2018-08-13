727d2a7176b4af34e12fb1a5c7277970211b02d4
Bhavya Srivastava <bhavya17037@iiitd.ac.in>
2018-08-13 22:49:17 +0530

    Fix typo in MainActivity.java
    Typo fix

--- app/src/main/java/com/example/android/stocks/MainActivity.java
+++ app/src/main/java/com/example/android/stocks/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         stockData[stockIndex][1] = data.getClose();
     }
 
-    // Make sure to call make all necessary http requests before calling populateListView
+    // Make sure to make all necessary http requests before calling populateListView
     // This should be called in the end, when all json data is parsed, and stockData is filled
     public void PopulateListView(){
 
